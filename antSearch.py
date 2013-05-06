ac2bdbfbb5289f71d9cf82cb0238c432d2454fcd
Ciarán Mooney <general.mooney@googlemail.com>
2013-05-06 11:28:48 +0100

    Started writing tests and getting them to pass.

--- antSearch.py
+++ antSearch.py
@@ -3,8 +3,8 @@
 import random
 
 class ant(object):
-    ''' Ants looks for food, when they find it they return home by the most
-        direct path. Leaving behind a trail of pheremones.
+    ''' Ants looks for food, when they find it they return home follwing a
+        random path. Leaving behind a trail of pheremones.
 
         Ants look randomly, unless they see a pheremone trail. If they see
         multiple trails they follow the strongest.
@@ -30,17 +30,26 @@ class world(object):
     ''' World contains the hive, the ants, and the food.
     '''
     def __init__(self, size, food='random', hive='random'):
-        self.world = []
-        random = hLocation
-        random = fLocation
+        self.world = [[point]*size]*size
+        if hive == 'random':
+            hLocation = (random.randint(0,size), random.randint(0,size))
+        else:
+            hLocation = hive
+
+        if food == 'random':
+            fLocation = (random.randint(0,size), random.randint(0,size))
+        else:
+            fLocation = food
+
         self.create_hive(hLocation)
         self.create_food(fLocation)
 
     def create_hive(self, hiveLocation):
         pass
 
-    def create_food(self, foodLocation):
-        pass
+    def create_food(self, foodLocation, amount=100):
+        x, y = foodLocation
+        self.world[x][y] = food(foodLocation, amount)
 
     def create_ants(self, number=100):
         pass
@@ -51,12 +60,36 @@ class world(object):
     def search_step(self):
         pass
 
+    def point(self, coords):
+        x, y = coords
+        return self.world[x][y]
+
 class point(object):
     ''' A point in the world. This keeps track of the pheremone trails.
         or food, or hive.
     '''
-    def __init__(self):
+    def __init__(self, location):
+        self.location = location
+        self.neighbours = []
         self.pheremones  = 0
     
     def pheremone_decay(self):
         self.pheremones -= 1
+
+    def find_neighbours(self):
+        self.neighbours.append(stuff)
+
+class hive(point):
+    pass
+
+class food(point):
+    '''
+    '''
+    def __init__(self, location, amount=100 ):
+        point.__init__(self, location)
+        print "amount =", amount
+        print location
+        self.foodLeft = amount
+
+    def remove_food(self):
+        self.foodLeft -= 1
--- test_antSearch.py
+++ test_antSearch.py
@@ -4,22 +4,36 @@ import unittest
 import antSearch
 
 class TestWorld(unittest.TestCase):
-
+    
     def setUp(self):
         pass
-
+    
     def test_create(self):
-        newWorld = antSearch.world(100)
-        assert(len(newWorld) == 100)
-        assert(len(newWorld[0] == 100)
+        World1 = antSearch.world(100)
+        self.assertEqual(len(World1.world), 100)
+        self.assertEqual(len(World1.world[0]), 100)
 
-class TestAnt(unittest.TestCase):
-    
-    def setUp(self):
-        pass
-    
-    def test_initialise(self):
-        new_ant = antSearch.ant() 
+    def test_create_food(self):
+        newWorld = antSearch.world(100, food=(1,1))
+        self.assertEqual(newWorld.point((12,13)), antSearch.point)
+        self.assertEqual(type(newWorld.point((1,1))), antSearch.food)
+        print(newWorld.point((1,1)))
+        food = newWorld.point((1,1))
+        print(food.foodLeft)
+        
+        self.assertEqual(newWorld.point((1,1)).foodLeft, 100)
+
+class TestPoint(unittest.TestCase):
+    pass
+
+#class TestAnt(unittest.TestCase):
+#    
+#    def setUp(self):
+#        pass
+#    
+#    def test_initialise(self):
+#        new_ant = antSearch.ant() 
 
 if __name__ == '__main__':
     unittest.main()
+
