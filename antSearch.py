4a5ac54c919aacb320c12c61de2d618a02677260
Ciarán <general.mooney@googlemail.com>
2014-07-17 13:20:55 +0100

    TODO Tidied
    Todo Tidied and added simulation TODO

--- antSearch.py
+++ antSearch.py
@@ -1,20 +1,21 @@
 #! /usr/bin/env python
 
 # TODO
-# [x] Decay should be a rate, need a step counter of world.
+# [] Write a 1x1, 2x1 and 3x1 1D simulation.
 # [] Re-read and comment all tests
 # [] Re-write tests to use the "setUP" function rather than creating new
 #    worlds each time.
 # [] Write food class - unnecessary
 # [] Write PyGame visuatilsation
-# [x] Make pheremones decay "independantly" depending on when they were
-#    deposited.
 # [] Write style guide. One line between ''' ''' and code, two lines between
 #    different functions
 # [] There should be pre-step, step, post-step functions for points, ants and 
 #    the world.
 # [] Document counting from zero.
 # [] Re-write fragile tests.
+# [x] Make pheremones decay "independantly" depending on when they were
+#    deposited.
+# [x] Decay should be a rate, need a step counter of world.
 
 import random
 from random import choice
